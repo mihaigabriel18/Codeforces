5352e6e749c442354b46e02ef59f58b371d79bbc
mihaigabriel18 <mihaicalitescu18@gmail.com>
2021-03-09 21:32:58 +0200

    10. solved

--- Codeforces Div.2, A/10. Beautiful Matrix/source.cpp
+++ Codeforces Div.2, A/10. Beautiful Matrix/source.cpp
@@ -4,4 +4,14 @@ using namespace std;
 
 int main() {
     int matrix[5][5];
+    int one_i, one_j;
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++) {
+            cin >> matrix[i][j];
+            if (matrix[i][j] == 1) {
+                one_i = i;
+                one_j = j;
+            }
+        }
+    cout << abs(one_i - 2) + abs(one_j - 2);
 }
