c5e0395f9df1fa6fa2f31b6263f11b695f07d704
mihaigabriel18 <mihaicalitescu18@gmail.com>
2021-03-10 14:31:53 +0200

    48 Solved

---
+++ Codeforces Div.2, A/48. Cut Ribbon/source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, a, b, c;
+    int val[3];
+    cin >> n >> a >> b >> c;
+
+    val[0] = a;
+    val[1] = b;
+    val[2] = c;
+
+    sort(val, val + 3);
+
+    a = val[0];
+    b = val[1];
+    c = val[2];
+
+    // if (n % a == 0) {
+    //     cout << n / a;
+    //     return 0;
+    // }
+    // else {
+
+    // }
+    int nr = 0, nr_a, nr_b, nr_c;
+    for (nr_c = 0; nr_c <= n / c; nr_c++) {
+        for (nr_b = 0; nr_b <= n / b; nr_b++) {
+            int nn = n - c * nr_c - b * nr_b;
+            if (nn >= 0 && nn % a == 0) {
+                nr_a = nn / a;
+                int nr_aux = nr_a + nr_b + nr_c;
+                if (nr_aux > nr)
+                    nr = nr_aux;
+            }
+        }
+    }
+    cout << nr;
+}
