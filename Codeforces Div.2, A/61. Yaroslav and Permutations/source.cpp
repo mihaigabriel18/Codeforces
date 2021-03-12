76a56e63d281710ba56ec6433efb70dd6728c6ea
mihaigabriel18 <mihaicalitescu18@gmail.com>
2021-03-13 01:36:32 +0200

    61 Solved

---
+++ Codeforces Div.2, A/61. Yaroslav and Permutations/source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int n;
+    map<int, int> nr;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        nr[num]++;
+    }
+
+    int max = INT16_MIN;
+    int sum = 0;
+    for (auto i : nr) {
+        sum += i.second;
+        if (i.second > max)
+            max = i.second;
+    }
+    sum -= max;
+
+    if (sum >= max - 1) {
+        cout << "YES";
+        return 0;
+    }
+    else {
+        cout << "NO";
+        return 0;
+    }
+}
