98137a404e9d74c9ed30d7206af122b6be677681
mihaigabriel18 <mihaicalitescu18@gmail.com>
2021-04-17 21:13:49 +0300

    Solved Multiplicity

--- Raw Codeforces/Multiplicity/source.cpp
+++ Raw Codeforces/Multiplicity/source.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-#define MAX_DIM 100001
+#define MAX_DIM 1000001
 #define MOD 1000000007
 
-long long current[MAX_DIM], sum[MAX_DIM];
+long long dp[MAX_DIM];
 int numbers[MAX_DIM];
+
+bool descending(int i, int j) { return i > j; }
+
 int main() {
     int n;
     cin >> n;
@@ -15,20 +20,31 @@ int main() {
         cin >> numbers[i];
     }
 
-    sum[0] = 1;
-
-    for (int global_index = 1; global_index < n; global_index++) {
-        current[0] = 1;
-        for (int local_index = 1; local_index <= global_index; local_index++)
-            current[local_index] = (numbers[global_index] % (local_index + 1) == 0) ? sum[local_index - 1] : 0;
+    dp[0] = 1;
+    for (int i = 0; i < n; i++) {
+        int nr = numbers[i];
+        // get al divisors of the number
+        vector<int> divisors;
         
-        for (int i = 0; i <= global_index; i++)
-            sum[i] += current[i];
+        for (int j = 1; j * j <= nr; j++) {
+            if (nr % j == 0) {
+                if (j * j == nr) {
+                    divisors.push_back(j); // push it only 1 time
+                } else {
+                    divisors.push_back(j);  // push it 2 times
+                    divisors.push_back(nr / j);
+                }
+            }
+        }
+        sort(divisors.begin(), divisors.end(), descending);
+        for (int j = 0; j < divisors.size(); j++) {
+            dp[divisors[j]] = (dp[divisors[j]] + dp[divisors[j] - 1]) % MOD;
+        }
     }
 
     long long sum_result = 0;
-    for (int i = 0; i < n; i++)
-        sum_result = (sum_result + sum[i]) % MOD;
+    for (int i = 1; i <= n; i++)
+        sum_result = (sum_result + dp[i]) % MOD;
 
     cout << sum_result;
 }
